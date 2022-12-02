d24533f0de6dbfdae77ec4fef0cc47e3ddd52ad7
alsruch02 <64555392+alsruch02@users.noreply.github.com>
2022-12-02 18:24:45 +0900

    priority Queue add
    priority Queue 추가함
    .h element 수정해야됨

---
+++ DataStruct_final_term/priorityQueue.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "stdafx.h"
+
+#define MAX_ELEMENT 400 
+
+typedef struct {
+	int key;
+} element;
+
+typedef struct {
+	element heap[MAX_ELEMENT];
+	int heap_size;
+} HeapType;
+
+HeapType* create();
+void init(HeapType* h);
+
+void insertMaxHeap(HeapType* h, element item);
+
+element deleteMaxHeap(HeapType* h);
