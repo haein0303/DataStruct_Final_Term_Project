3eea69ce71d7001b6a5c73dabc473afce36af104
seil0706 <114226090+seil0706@users.noreply.github.com>
2022-12-02 17:56:55 +0900

    add stack
    stack h add  ,stack cpp add

---
+++ DataStruct_final_term/stack.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+
+
+
+
+
+
+void init_stack(StackType *s) {
+
+	s->top = -1;
+}
+
+int qis_empty(StackType* s) {
+	return (s->top == -1);
+}
+
+int is_full(StackType* s) {
+	return (s->top == (MAX_STACK_SIZE - 1));
+}
+
+void push(StackType* s, element item) {
+
+	if (is_full(s)) {
+		fprintf(stderr, "스택 포화 에러 \n");
+		return;
+	}
+	else s->data[++(s->top)] = item;
+}
+
+element pop(StackType* s) {
+	if (qis_empty(s)) {
+		fprintf(stderr, "스팩 공백 에러\n");
+		exit(1);
+	}
+	else return s->data[(s->top)--];
+}
+
+element peek(StackType* s) {
+	if (qis_empty(s)) {
+		fprintf(stderr, "스팩 포화 에러\n");
+			exit(1);
+	}
+	else return s->data[s->top];
+}
+
+
